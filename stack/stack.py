b016a86fe18e460378ded4ce00c1d995381c3fb7
Dylan Collins <dcollins5590@students.mc3.edu>
2020-07-13 21:24:37 -0400

    finished MVP for assignment

--- stack/stack.py
+++ stack/stack.py
@@ -1,3 +1,7 @@
+from singly_linked_list2 import LinkedList
+from singly_linked_list2 import Node
+
+
 """
 A stack is a data structure whose primary purpose is to store and
 return elements in Last In First Out order. 
@@ -13,13 +17,48 @@ return elements in Last In First Out order.
 class Stack:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+        self.storage = LinkedList()
 
     def __len__(self):
-        pass
-
+        current_node = self.storage.head
+        length_of_storage = 0
+        while current_node:
+            length_of_storage = length_of_storage + 1
+            current_node = current_node.get_next()
+        return length_of_storage
     def push(self, value):
-        pass
-
+        self.storage.add_to_tail(value)
     def pop(self):
-        pass
+        return self.storage.remove_tail()
+
+
+# Using an array
+# class Stack:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = []
+
+#     def __len__(self):
+#         return len(self.storage)
+
+#     def push(self, value):
+#         self.storage.append(value)
+#     def pop(self):
+#         if len(self.storage) == 0:
+#             return None
+#         else:
+#             last_element = self.storage[-1]
+#             self.storage.pop()
+#             return last_element
+
+
+
+if __name__ == '__main__':
+    my_stack = Stack()
+    print(len(my_stack))
+    my_stack.push(1)
+    my_stack.push(2)
+    # my_stack.push(3)
+    print(len(my_stack))
+    my_stack.pop()
+    print(len(my_stack))
